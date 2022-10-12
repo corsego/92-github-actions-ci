b973cf7d61966d6b06267146b443fc56d85eaa5c
yshmarov <yshmarov@gmail.com>
2022-10-12 15:49:58 +0200

    update routes, add system tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
-  get 'static_pages/pricing'
+  root 'static_pages#landing_page'
+  get 'pricing', to: 'static_pages#pricing'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get landing_page" do
-    get static_pages_landing_page_url
+    get root_url
     assert_response :success
   end
 
   test "should get pricing" do
-    get static_pages_pricing_url
+    get pricing_url
     assert_response :success
   end
 end
