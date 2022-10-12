d48accc09a7f873b2c6f94caea215f7f00155908
yshmarov <yshmarov@gmail.com>
2022-10-12 15:46:10 +0200

    generate static_pages controller

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get landing_page" do
+    get static_pages_landing_page_url
+    assert_response :success
+  end
+
+  test "should get pricing" do
+    get static_pages_pricing_url
+    assert_response :success
+  end
+end
